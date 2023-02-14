896eae0025d694b132e6cd2ed0a2a1df45e332f4
vladmsnk <moiseenkov-v@mail.ru>
2023-02-14 14:06:40 +0300

    solved lru cashe

--- newera/lru.go
+++ newera/lru.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type LRUCache struct {
 	Storage  map[int]*Pair
 	Cache    *Pair
@@ -22,93 +24,170 @@ func Constructor(capacity int) LRUCache {
 }
 
 func (this *LRUCache) Get(key int) int {
+
 	currPtr, ok := this.Storage[key]
 	if !ok {
 		return -1
 	}
-	//cut its current location
-	currPtrPrev := currPtr.Prev
-	currPtrNext := currPtr.Next
-	currPtrPrev.Next = currPtrNext
-	currPtrNext.Prev = currPtrPrev
+	//Move to the beginning
+	if len(this.Storage) > 1 && currPtr != this.Cache {
 
-	//move to the beginning
-	this.Cache.Prev = currPtr
-	currPtr.Next = this.Cache
-	this.Cache = this.Cache.Prev
+		prevNode := currPtr.Prev
+		nextNode := currPtr.Next
+
+		prevNode.Next = nextNode
+		nextNode.Prev = prevNode
+
+		currPtr.Next = this.Cache
+		currPtr.Prev = this.Head
+
+		this.Cache.Prev = currPtr
+		this.Head.Next = currPtr
+
+		this.Cache = currPtr
+	}
 
 	return currPtr.Val
 }
 
 func (this *LRUCache) Put(key int, value int) {
-
+	if this.Capacity == 0 {
+		return
+	}
 	pairPtr := &Pair{Key: key, Val: value}
 
 	if this.Cache == nil {
 		this.Cache = pairPtr
-		//nil <-H-> *Pair{} <-B-> nil
-		this.Head.Next = pairPtr
-		pairPtr.Prev = this.Head
-
-		//maybe useless
-		this.Back.Prev = pairPtr
-		pairPtr.Next = this.Back
-
-		this.Storage[key] = pairPtr
+		//H <- *Pair{} -> B
+		this.Cache.Next = this.Back
+		this.Cache.Prev = this.Head
+		//H <-> *Pair{} <-> B
+		this.Head.Next = this.Cache
+		this.Back.Prev = this.Cache
+		// store pointer in map
+		this.Storage[key] = this.Cache
 		return
 	}
 
 	_, ok := this.Storage[key]
 	if !ok {
-		this.Storage[key] = pairPtr
-
-		//curPtr
-		this.Cache.Prev = pairPtr
+		// add to the beginning
 		pairPtr.Next = this.Cache
-
-		//maybe useless
 		pairPtr.Prev = this.Head
+
+		this.Cache.Prev = pairPtr
 		this.Head.Next = pairPtr
 
-		this.Cache = this.Cache.Prev
+		this.Cache = pairPtr
 
-		//nil <-H-> *Pair{1, 3} -> *Pair{1, 2} -> *Pair{1, 1}  <-B-> nil
+		this.Storage[key] = pairPtr
+
 		if len(this.Storage) > this.Capacity {
+
 			preLast := this.Back.Prev.Prev
-			key := preLast.Next.Key
+			keyToDelete := preLast.Next.Key
 
 			preLast.Next = this.Back
 			this.Back.Prev = preLast
-			delete(this.Storage, key)
+			delete(this.Storage, keyToDelete)
 		}
 
 	} else { // if smth with this key already exists
 		// just set new value and move this to head
+
 		currPtr := this.Storage[key]
+		currPtr.Val = value
 
-		//delete this ptr from list
-		currPtrPrev := currPtr.Prev
-		currPtrNext := currPtr.Next
-		currPtrPrev.Next = currPtrNext
-		currPtrNext.Prev = currPtrPrev
+		if len(this.Storage) > 1 && currPtr != this.Cache {
+			//swap
+			prevNode := currPtr.Prev
+			nextNode := currPtr.Next
 
-		//replace this ptr with newvalue one
-		this.Storage[key] = pairPtr
+			prevNode.Next = nextNode
+			nextNode.Prev = prevNode
 
-		//push this new ptr to the beginning of the list
-		//this.Cache points to beginning
-		this.Cache.Prev = pairPtr
-		pairPtr.Next = this.Cache
+			currPtr.Next = this.Cache
+			currPtr.Prev = this.Head
+			this.Cache.Prev = currPtr
+			this.Head.Next = currPtr
 
-		pairPtr.Prev = this.Head
-		this.Head.Next = pairPtr
-		this.Cache = this.Cache.Prev
+			this.Cache = currPtr
+		}
 	}
-
 }
-
 func main() {
-	Constructor(3)
+	lru := Constructor(3)
+	lru.Put(1, 1)
+	lru.Put(2, 2)
+	lru.Put(3, 4)
+
+	lru.Put(2, 2)
+	lru.Put(5, 5)
+	lru.Put(6, 6)
+	lru.Put(7, 7)
+	lru.Put(8, 8)
+
+	fmt.Println("FirstNode: ", lru.Cache.Key, " : ", lru.Cache.Val)
+	fmt.Println("SecondNode: ", lru.Cache.Next.Key, " : ", lru.Cache.Next.Val)
+	fmt.Println("ThirdNode: ", lru.Cache.Next.Next.Key, " : ", lru.Cache.Next.Next.Val)
+
+	val := lru.Get(8)
+	val = lru.Get(8)
+	fmt.Println(val)
+
+	fmt.Println("FirstNode: ", lru.Cache.Key, " : ", lru.Cache.Val)
+	fmt.Println("SecondNode: ", lru.Cache.Next.Key, " : ", lru.Cache.Next.Val)
+	fmt.Println("ThirdNode: ", lru.Cache.Next.Next.Key, " : ", lru.Cache.Next.Next.Val)
+
+	//FirstNode:  3  :  2
+	//SecondNode:  3  :  2
+	//
+
 }
 
-
+//func main() {
+//	lru := Constructor(3)
+//	lru.Put(1, 1)
+//	lru.Put(1, 2)
+//	fmt.Println("____Test1, replace value by key____")
+//	//lru.Put(1, 2)
+//	fmt.Println("List: ", lru.Cache.Key, " : ", lru.Cache.Val)
+//
+//	currKey := lru.Cache.Key
+//	fmt.Println("Map: ", lru.Storage[currKey].Key, " : ", lru.Storage[currKey].Val)
+//
+//	fmt.Println("____Test2, add several nodes____")
+//	//Nodes that were added should be in the beginning
+//	//*Pair{3, 4} should be at the first place and *Pair{2, 3} at the second
+//	lru.Put(2, 3)
+//	lru.Put(3, 4)
+//	fmt.Println("FirstNode: ", lru.Cache.Key, " : ", lru.Cache.Val)
+//	fmt.Println("SecondNode: ", lru.Cache.Next.Key, " : ", lru.Cache.Next.Val)
+//	fmt.Println("ThirdNode: ", lru.Cache.Next.Next.Key, " : ", lru.Cache.Next.Next.Val)
+//
+//	fmt.Println("____Test3, add node so that the capacity will be stepped over ____")
+//	//Node *Pair{4, 6} should be in the first place.
+//	//Node *Pair{1, 2}  should be deleted
+//	lru.Put(4, 6)
+//	fmt.Println("FirstNode: ", lru.Cache.Key, " : ", lru.Cache.Val)
+//	fmt.Println("SecondNode: ", lru.Cache.Next.Key, " : ", lru.Cache.Next.Val)
+//	fmt.Println("ThirdNode: ", lru.Cache.Next.Next.Key, " : ", lru.Cache.Next.Next.Val)
+//
+//	fmt.Println("____Test4, add node so that the capacity will be stepped over again____")
+//	// First node is Pair{5, 7}
+//	// Second node is Pair{4, 6}
+//	// Third node is Pair{3, 4}
+//	lru.Put(5, 7)
+//	fmt.Println("FirstNode: ", lru.Cache.Key, " : ", lru.Cache.Val)
+//	fmt.Println("SecondNode: ", lru.Cache.Next.Key, " : ", lru.Cache.Next.Val)
+//	fmt.Println("ThirdNode: ", lru.Cache.Next.Next.Key, " : ", lru.Cache.Next.Next.Val)
+//
+//	fmt.Println("____Test5, get existing key____")
+//	val := lru.Get(3)
+//	//val should be 4
+//	fmt.Println("val :", val)
+//	fmt.Println("FirstNode: ", lru.Cache.Key, " : ", lru.Cache.Val)
+//	fmt.Println("SecondNode: ", lru.Cache.Next.Key, " : ", lru.Cache.Next.Val)
+//	fmt.Println("ThirdNode: ", lru.Cache.Next.Next.Key, " : ", lru.Cache.Next.Next.Val)
+//
+//}
