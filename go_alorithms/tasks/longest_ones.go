4c5e7a734fe5652602f69cc1ae56cc3b029509b0
vladmsnk <moiseenkov-v@mail.ru>
2023-02-20 10:38:03 +0300

    max consecutive ones 3

--- go_alorithms/tasks/longest_ones.go
+++ go_alorithms/tasks/longest_ones.go
@@ -1,1 +1,26 @@
 package tasks
+
+import "math"
+
+// [1,1,1,0,0,0,1,1,1,1,0]
+
+func longestOnes(nums []int, k int) int {
+	currK := k
+	maxOnes := 0
+
+	for l, r := 0, 0; r < len(nums); {
+		if nums[r] == 1 || currK > 0 {
+			if nums[r] == 0 {
+				currK--
+			}
+			r++
+			maxOnes = int(math.Max(float64(maxOnes), float64(r-l)))
+		} else {
+			if nums[l] == 0 {
+				currK++
+			}
+			l++
+		}
+	}
+	return maxOnes
+}
